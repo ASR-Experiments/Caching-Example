7788748ed682896488800e64d052ed7d8ee9d96e
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2022-12-04 20:06:09 +0530

    Added comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
+//    Important
     java
     id("org.springframework.boot") version "2.7.6"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+//    Can be ignored
     id("org.sonarqube") version "3.5.0.2730"
     id("org.springdoc.openapi-gradle-plugin") version "1.5.0"
 }
@@ -30,14 +32,19 @@ sonarqube {
 }
 
 dependencies {
+//    Mandatory
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-rest")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("com.mysql:mysql-connector-j")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+//    Specific to MySQL
+    runtimeOnly("com.mysql:mysql-connector-j")
+//    Can be ignored
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+//    Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+//    Open API Spec
     implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
     implementation("org.springdoc:springdoc-openapi-data-rest:1.6.13")
     implementation("org.springdoc:springdoc-openapi-javadoc:1.6.13")
