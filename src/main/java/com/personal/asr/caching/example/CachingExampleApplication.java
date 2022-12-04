5f463113482f8eeb5dad6feeb18f5d54e5429851
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2022-12-04 18:37:56 +0530

    Added boilerplate code

---
+++ src/main/java/com/personal/asr/caching/example/CachingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.personal.asr.caching.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/asr/caching/example/CachingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.asr.caching.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
